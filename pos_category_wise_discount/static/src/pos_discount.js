dbcdb46a0a4e76e41c88cba2218d6abc53ad9ed6
Nishal <nishalkkn@gmail.com>
2024-09-30 18:57:12 +0530

    sep 30

--- pos_category_wise_discount/static/src/pos_discount.js
+++ pos_category_wise_discount/static/src/pos_discount.js
@@ -2,11 +2,41 @@
 
 import { patch } from "@web/core/utils/patch";
 import { Order } from "@point_of_sale/app/store/models";
-
+import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 patch(Order.prototype, {
     async pay() {
-        console.log('Discount limit :', this.pos.config.pos_discount_limit_bool);
-//        console.log('Discount limit value :', this.pos.config.pos_disc_limit);
-//        console.log('config :', this.pos.config);
+        if (this.pos.config.is_pos_discount_limit) {
+            let dictionary = {}
+            var discount_limit = this.pos.config.pos_discount_limit
+            for (var i = 0; i < this.orderlines.length; i++) {
+                var product_categ_id = this.orderlines[i].product.pos_categ_ids
+                var discount = this.orderlines[i].price * (this.orderlines[i].discount / 100)
+                if (product_categ_id in dictionary) {
+                    dictionary[product_categ_id] += discount
+                } else {
+                    dictionary[product_categ_id] = discount
+                }
+            }
+            var keys = Object.keys(dictionary)
+            for (var i = 0; i < keys.length; i++) {
+                if ((dictionary[keys[i]]) > discount_limit) {
+                    var limit = 1
+                }
+            }
+            if (limit == 1) {
+                 this.env.services.popup.add(ErrorPopup, {
+                    title: "Discount limit",
+                    body: "Category wise discount limit reached !!!",
+                });
+            } else {
+                return {
+                    ...super.pay(...arguments)
+                }
+            }
+        } else {
+            return {
+                ...super.pay(...arguments)
+            }
+        }
     },
 });
