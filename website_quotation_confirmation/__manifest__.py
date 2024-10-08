0f9371e75add6bb9f0a306a356bc15a8777d3cec
Nishal <nishalkkn@gmail.com>
2024-10-08 17:32:12 +0530

    oct 8

---
+++ website_quotation_confirmation/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Website Quotation Confirmation",
+    'application': True,
+    'version': '17.0.1.0.1',
+    'summary': 'Website Quotation Confirmation',
+    'description': """
+
+    """,
+
+    'depends': [
+        'base',
+        'sale'
+    ],
+
+    'data': [
+        'views/sale_portal_templates.xml',
+    ],
+
+    'license': 'LGPL-3',
+
+}
